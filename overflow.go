bd1412b764bc9ae25f8271de8e76cacb0a614cc0
Alexandr Stefurishin <alexandr.stefurishin@flant.com>
2024-12-09 14:25:56 +0300

    Convert function, minor improvements (#1)
    * Convert function, minor improvements
    
    * fix readme
    
    * Should probably just retain the original license without adding all contributors

--- overflow.go
+++ overflow.go
@@ -1,10 +1,10 @@
 package overflow
 
-type number interface {
+type Int interface {
 	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64
 }
 
-func Add[T number](a, b T) (T, bool) {
+func Add[T Int](a, b T) (T, bool) {
 	c := a + b
 	if (c > a) == (b > 0) {
 		return c, true
@@ -12,7 +12,7 @@ func Add[T number](a, b T) (T, bool) {
 	return c, false
 }
 
-func Sub[T number](a, b T) (T, bool) {
+func Sub[T Int](a, b T) (T, bool) {
 	c := a - b
 	if (c < a) == (b > 0) {
 		return c, true
@@ -20,7 +20,7 @@ func Sub[T number](a, b T) (T, bool) {
 	return c, false
 }
 
-func Mul[T number](a, b T) (T, bool) {
+func Mul[T Int](a, b T) (T, bool) {
 	if a == 0 || b == 0 {
 		return 0, true
 	}
@@ -32,15 +32,25 @@ func Mul[T number](a, b T) (T, bool) {
 	}
 	return c, false
 }
-func Div[T number](a, b T) (T, bool) {
+
+func Div[T Int](a, b T) (T, bool) {
 	q, _, ok := Quotient(a, b)
 	return q, ok
 }
-func Quotient[T number](a, b T) (T, T, bool) {
+
+func Quotient[T Int](a, b T) (T, T, bool) {
 	if b == 0 {
 		return 0, 0, false
 	}
 	c := a / b
-	status := (c < 0) == ((a < 0) != (b < 0))
-	return c, a % b, status
+	ok := (c < 0) == ((a < 0) != (b < 0))
+	return c, a % b, ok
+}
+
+func Convert[T Int, U Int](a T) (U, bool) {
+	b := U(a)
+	if T(b) == a && (a < 0) == (b < 0) {
+		return b, true
+	}
+	return b, false
 }
